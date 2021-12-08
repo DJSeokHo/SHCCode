2cf1c62c2a4c86a0c9436dad86f65a1790286ace
CodingWithCat <djseokho@vip.qq.com>
2021-12-08 12:12:44 +0900

    Seok Ho

---
+++ foundation_cpp/11_C++面向对象/2_继承/继承.cpp
@@ -0,0 +1,50 @@
+/*
+
+同一个类	yes	yes	yes
+派生类	yes	yes	no
+外部的类	yes	no	no
+
+*/
+#include <iostream>
+ 
+using namespace std;
+ 
+// 基类
+class Shape 
+{
+    public:
+        void setWidth(int w)
+        {
+            width = w;
+        }
+        void setHeight(int h)
+        {
+            height = h;
+        }
+    protected:
+        int width;
+        int height;
+};
+ 
+// 派生类
+class Rectangle: public Shape
+{
+    public:
+        int getArea()
+        { 
+            return (width * height); 
+        }
+};
+ 
+int main(void)
+{
+    Rectangle Rect;
+    
+    Rect.setWidth(5);
+    Rect.setHeight(7);
+    
+    // 输出对象的面积
+    cout << "Total area: " << Rect.getArea() << endl;
+    
+    return 0;
+}
